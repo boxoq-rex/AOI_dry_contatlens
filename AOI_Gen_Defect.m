60210e5585354f151c60c06b4b60a14fa5b49256
boxoq-rex <boxoq.rex@gmail.com>
2017-04-04 18:17:47 +0800

    Read image
    Read the image of the perfect contact lens

--- AOI_Gen_Defect.m
+++ AOI_Gen_Defect.m
@@ -3,7 +3,25 @@ clc; clear all; close all;
 
 % Definition
 
-% Read perfect contact lense image
+% == Read perfect contact lense image ==
+% Write the path of the image
+pathd = 'img\';
+num_imgs = length(dir([pathd '*.jpg']));
+img_dir = dir([pathd '*.jpg']);
+
+% Read the image of the perfect contact
+for d = 1:num_imgs
+    img_obj = img_dir(d); % Read No.d image object
+    img_ori = imread([pathd img_obj.name]);
+    figure, imshow(img_ori);
+end
+
 
 % Find the center of the lense in a given perfect image
+% [?] Need to modify the function for calculating the center and the radius
+% of the given image of perfect contact
+xc = 659.2985*1.75;
+yc = 584.7214*1.75;
+rc = 544.1864*1.75;
+
 
