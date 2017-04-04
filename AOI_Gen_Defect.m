27ac30391adf4cd6afc461f98241473eb7dec898
boxoq-rex <boxoq.rex@gmail.com>
2017-04-04 20:10:33 +0800

    Fix x-axis out-of-range
    When extracting the size of an image, be careful of which one is x or
    y.

--- AOI_Gen_Defect.m
+++ AOI_Gen_Defect.m
@@ -7,8 +7,8 @@ clc; clear all; close all;
 % (DEGREE_END - DEGREE_START + 1) = N*DEGREE_INTERVAL where N is an integer.
 NUM_LOOP = 1; 
 DEGREE_START = 0;   
-DEGREE_END = 44;
-DEGREE_INTERVAL = 1;
+DEGREE_END = 359;
+DEGREE_INTERVAL = 45;
 CROP_SIZE = 150; % Size of each generated defect image (Even number)
 
 %% Read perfect contact lense image
@@ -21,7 +21,7 @@ img_dir = dir([pathd '*.jpg']);
 for d = 1:num_imgs          % Count for number of perfect images to read
     img_obj = img_dir(d);   % Read No.d image object
     img_ori = imread([pathd img_obj.name]);
-    [img_size_x img_size_y img_size_z] = size(img_ori); 
+    [img_size_y img_size_x img_size_z] = size(img_ori); 
     % figure, imshow(img_ori); % Test for img_ori 
     
     % Find the center of the lense in a given perfect image
@@ -34,7 +34,7 @@ for d = 1:num_imgs          % Count for number of perfect images to read
     for i = 1:NUM_LOOP % See Definition
         for j = DEGREE_START:DEGREE_INTERVAL:DEGREE_END % See Definition
         
-        % Generate the start and end point of the generated defects
+        %% Generate the start and end point of the generated defects
         % (START_POINT, END_POINT) where START_POINT < END_POINT
         END_POINT = unidrnd(CROP_SIZE,1,1);
         if END_POINT < 2  
@@ -42,27 +42,43 @@ for d = 1:num_imgs          % Count for number of perfect images to read
         end
         START_POINT = unidrnd(END_POINT-1,1,1);    
             
-        % Crop CROP_SIZExCROP_SIZE-images from img_ori
+        %% Crop CROP_SIZExCROP_SIZE-images from img_ori
         % For example, 150x150-image
         
         % Definition of BBOX coordination
         CROP_IMG_X = xc + rc*cos(2*pi*j/360) - CROP_SIZE/2;
         CROP_IMG_Y = yc - rc*sin(2*pi*j/360) - CROP_SIZE/2;
+        
+        
         % Modification for out-of-range situation
-        ORI_IMG_X = img_size_x; % haven't define yet ***
-        ORI_IMG_Y = img_size_y; % haven't define yet ***
+        ORI_IMG_X = img_size_x; 
+        ORI_IMG_Y = img_size_y;
         EDGE_DEL_X = abs(CROP_IMG_X + CROP_SIZE - ORI_IMG_X);
         EDGE_DEL_Y = abs(CROP_IMG_Y + CROP_SIZE - ORI_IMG_Y);
         
-        %Test
-        CROP_IMG_X = 2000;
-        
+        % Check if CROP_IMG_X is out-of-range
         if CROP_IMG_X + CROP_SIZE > ORI_IMG_X
             CROP_IMG_X = CROP_IMG_X - EDGE_DEL_X;
         elseif CROP_IMG_X < 0
             CROP_IMG_X = 0;
+        end        
+
+        % Check if CROP_IMG_Y is out-of-range
+        if CROP_IMG_Y + CROP_SIZE > ORI_IMG_Y
+            CROP_IMG_Y = CROP_IMG_Y - EDGE_DEL_Y;
+        elseif CROP_IMG_Y < 0
+            CROP_IMG_Y = 0;
         end
-            
+        
+        % Crop a CROP_SIZExCROP_SIZE image from the original image
+        img_crop = imcrop(img_ori,[CROP_IMG_X CROP_IMG_Y 149 149]);
+        figure, imshow(img_crop);
+        bbox = [CROP_IMG_X CROP_IMG_Y 149 149];
+        
+        img_test = insertShape(img_ori,'Rectangle',bbox,'LineWidth',2, 'Color', 'yellow');
+        figure, imshow(img_test); 
+        
+        
         end % j
     end % i
 
